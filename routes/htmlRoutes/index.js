354c1ef812d16c9ef9f9e737618563ad145a584f
Tye Stanley <tyestanley090@gmail.com>
2022-06-30 14:16:24 -0500

    setup htmlRoutes

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const router = require('express').Router();
+
+// HTML GET Requests to inde.html
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// HTML GET Requests to notes.html
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// Sets the catch all route to render the index.html page
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// Export the router
+module.exports = router;
